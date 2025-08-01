267fab5e6fccb46862aee1cc9d48364c2d2d854a
I-am-powerman <seredkin.dmitrii.powerman@gmail.com>
2025-08-01 12:54:11 +0800

    2025-08-01

--- Driver_sql.py
+++ Driver_sql.py
@@ -1,10 +1,11 @@
 import psycopg2
 
-class driver_sql:
+class Driver_sql:
     def __init__(self, name_DB:str, name_host:str, user_name:str,
                      password: str, port:str):
         self.conn = self._connect_DB(name_DB, name_host, user_name,
                      password, port)
+        self.cur = self._cursor(self.conn)
 
     # подключение к базе данных, метод инициализируется в init
     # и передает параметр conn который используется в выполнении
@@ -15,20 +16,27 @@ class driver_sql:
                                 user=user_name, 
                                 password=password, 
                                 port=port)
-
+    
         return conn
+    
+    def _cursor(self, conn):
+        cur = conn.cursor()
+
+        return cur
 
     # функция выполненения SQL запроса
     # get_data нужен для SQL запроса на получение данных
-    def perform_sql(self, SQL_request: str, dict_values: dict = None, get_data: bool = False):
-        cur = self.conn.cursor()
-        cur.execute(SQL_request, dict_values)
+    def perform_sql(self, SQL_request: str, dict_values: dict = None):
         
-        if get_data:
-            # пока сделал чтобы он все данные доставал, но нужно сделать целенаправленный поиск
-            return cur.fetchall()
+        self.cur.execute(SQL_request, dict_values)
         
-    def close_DB(self):
+    # теперь эта функция вытаскивает объект по названию параметра
+    def get_data(self, SQL_request: str, condition:str):
+        self.cur.execute(SQL_request, condition)
+        data = self.cur.fetchall()
+        return data
+    # эта функция закрывает БД
+    def close_DB(self, ):
         self.conn.commit()
         self.conn.cursor().close()
         self.conn.close()
--- trash.py
+++ trash.py
@@ -1,8 +1,8 @@
-from protect import protect
+from Protect import Protect
 
 spisok = [1, 2, 3, 4, "slhbvos"]
 
-values = protect(spisok)
+values = Protect(spisok)
 list_keys = values.keys_list
 
 print(list_keys)
