ed4d60d2cb02b3420029c26d2dfe3d4f7b333904
I-am_powerman <seredkin.dmitrii.powerman@gmail.com>
2025-12-25 09:36:07 +0800

    trial_1

--- app/Data_Base/WorkDB.py
+++ app/Data_Base/WorkDB.py
@@ -1,11 +1,11 @@
-from connectDB import Get_engine_session_DB
-from model import Foto
+# from connectDB import Get_engine_session_DB
+# from model import Foto
 
-connectDB = Get_engine_session_DB('base_foto.db')
-engine = connectDB.get_engine()
-session = connectDB.get_sessoin()
+# connectDB = Get_engine_session_DB('base_foto.db')
+# engine = connectDB.get_engine()
+# session = connectDB.get_sessoin()
 
 
-foto = Foto(name_foto=name_foto, path=path)
-session.add(foto)
-session.commit()
+# foto = Foto(name_foto=name_foto, path=path)
+# session.add(foto)
+# session.commit()
--- app/web_interface/web.py
+++ app/web_interface/web.py
@@ -1,40 +1,44 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.responses import FileResponse
-from database.SQL_request import *
-from database.Connection import connection
+from app.Data_Base.connectDB import Get_engine_session_DB
+from app.Data_Base.model import Foto
 from typing import List
+import os
 import uvicorn
 
 app = FastAPI()
 
-@app.post("/file")
-def root(files: List[UploadFile] = File(...)):
-    table_images = connection()
-    name_colum = ["name_image"]
-    get_query = SQL_request("images", name_colum).req_obtain()
-    name_dict = table_images.get_data(get_query)
+# Папка для загрузки файлов
+UPLOAD_DIR = "uploads"
 
+@app.post("/upload")
+def root(files: List[UploadFile] = File(...),
+         name_dir: str = Form(...)):
+    
+    # Создаем папку с указанным именем
+    upload_path = os.path.join(UPLOAD_DIR, name_dir)
+    os.makedirs(upload_path, exist_ok=True)
+    
+    # работа с БД
+    connectDB = Get_engine_session_DB('../app/Data_Base/base_foto.db')
+    engine = connectDB.get_engine()
+    session = connectDB.get_sessoin()   
+
+    # работа с файлами
     for file in files:
         file_content = file.file.read()
         file_name = file.filename
-        path = f"images/{file_name}"
-        if file_name in name_dict.keys():
-            continue
+        path = f"{upload_path}/{file_name}"
+
         if not file_name:
             continue
         with open(path, "wb") as f:
             f.write(file_content)
-        name_colums = ["name_image", "path"]
-        values = [file_name, path]
 
-        Query = SQL_request("images", name_colums, values)
-
-        insert_query_sql = Query.req_insert()
-        insert_query_values = Query.dict_values()
-
-        table_images.perform_sql(insert_query_sql, insert_query_values)
-
-    table_images.close_DB()
+        # запись в БД
+        foto = Foto(name_foto=file_name, path=path)
+        session.add(foto)
+        session.commit()
 
 
 @app.get("/")
